6823ce5295f6d2f86a911295fb0ce429f852fff0
chrisna wibisono <chrisnawibisono31@gmail.com>
2019-10-01 18:46:34 +0700

    result interface

--- app/src/main/java/com/ulul/planetpedia/MainActivity2.java
+++ app/src/main/java/com/ulul/planetpedia/MainActivity2.java
@@ -1,29 +1,23 @@
 package com.ulul.planetpedia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
 
     Button btn_yes;
     Button btn_no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         btn_yes = (Button) findViewById(R.id.btn_yes);
         btn_no = (Button) findViewById(R.id.btn_no);
 
 //        btn_yes.setOnClickListener(this);
 //        btn_no.setOnClickListener(this);
-    }
-    public void Pindah(View view) {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-        startActivity(intent);
     }
 }
