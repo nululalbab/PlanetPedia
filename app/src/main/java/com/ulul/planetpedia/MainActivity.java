7a94b8513556536d6e4c2f8efa2b42131c9b806b
adeliarnur <ahmadeliar@gmail.com>
2019-10-01 16:05:59 +0700

    nambah result interface

--- app/src/main/java/com/ulul/planetpedia/MainActivity.java
+++ app/src/main/java/com/ulul/planetpedia/MainActivity.java
@@ -3,12 +3,21 @@ package com.ulul.planetpedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_yes;
+    Button btn_no;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_yes = (Button) findViewById(R.id.btn_yes);
+        btn_no = (Button) findViewById(R.id.btn_no);
+
+//        btn_yes.setOnClickListener(this);
+//        btn_no.setOnClickListener(this);
     }
 }
